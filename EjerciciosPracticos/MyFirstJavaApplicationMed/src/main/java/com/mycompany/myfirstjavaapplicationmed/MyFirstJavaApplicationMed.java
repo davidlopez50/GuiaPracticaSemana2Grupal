0a3ae4ea0b440831dd9de25061ede5249d70625a
david <david@AsusVivoBookX>
2026-07-28 14:16:55 -0600

    Primer Commit

---
+++ EjerciciosPracticos/MyFirstJavaApplicationMed/src/main/java/com/mycompany/myfirstjavaapplicationmed/MyFirstJavaApplicationMed.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.myfirstjavaapplicationmed;
+
+/**
+ *
+ * @author david
+ */
+public class MyFirstJavaApplicationMed {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
