3209593086b1e10684e4fdd8ded9299c12bdb7dd
david <david@AsusVivoBookX>
2026-07-28 15:38:40 -0600

    Primer Commit

---
+++ EjerciciosPracticos/CalculoCubo/src/main/java/com/mycompany/calculocubo/CalculoCubo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculocubo;
+
+import java.util.Scanner;
+/**
+ *
+ * @author david
+ */
+public class CalculoCubo {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double Arista;
+        double Volumen;
+        System.out.println("Ingrese el numero de aristas del cubo: ");
+        Arista = sc.nextDouble();
+        Volumen = Arista * Arista * Arista;
+        System.out.printf("Se calculo el volumen: %.2f%n", Volumen);
+    }
+}
