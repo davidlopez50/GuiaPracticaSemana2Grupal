b658ba93ce548a0a7c6cd950b2a0b6cec0cc20d2
david <david@AsusVivoBookX>
2026-08-11 19:18:15 -0600

    Pruebagrupal

---
+++ TareaPracticas/Pruebitasjava/src/main/java/com/mycompany/pruebitasjava/Pruebitasjava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebitasjava;
+
+/**
+ *
+ * @author david
+ */
+public class Pruebitasjava {
+
+    public static void main(String[] args) {
+        System.out.println("Buenasss chavales");
+    }
+}
